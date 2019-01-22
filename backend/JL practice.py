db42857d15ac65e10579fe32c509f0de981174c7
Jace <32490493+jacelecomte@users.noreply.github.com>
2019-01-22 18:52:04 -0500

    test
    test

--- backend/JL practice.py
+++ backend/JL practice.py
@@ -1,1 +1,7 @@
+#import pandas_datareader as pdr
+#import datetime
+#aapl = pdr.get_data_yahoo('AAPL',
+#                          start=datetime.datetime(2016, 1, 1),
+#                         end=datetime.datetime(2019, 1, 1))
 
+#from stocker import Stocker
--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,5 @@
-
+import pandas_datareader as pdr
+import datetime
+aapl = pdr.get_data_yahoo('AAPL',
+                          start=datetime.datetime(2016, 1, 1),
+                          end=datetime.datetime(2019, 1, 1))
