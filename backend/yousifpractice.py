878dc3770e2651e55280ab1e8d4bf310306823ca
y2km3 <yousif.kashat@gmail.com>
2019-01-22 18:51:22 -0500

    imported libraries and AAPL stock
    tried practicing with AAPL stock

--- backend/yousifpractice.py
+++ backend/yousifpractice.py
@@ -1,4 +1,13 @@
 import datetime as dt
+import matplotlib.pyplot as plt
+from matplotlib import style
+import pandas as pd
+import pandas_datareader.data as web
 
+style.use('ggplot')
 
+start = dt.datetime(2019, 1, 1)
+end = dt.datetime.now()
+df = web.DataReader("AAPL", 'yahoo', start, end)
 
+print (df)
